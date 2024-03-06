f4100debfeca4e919ffaf0065a8560123dac0ccd
Kapil Gupta <navkapil@gmail.com>
2024-03-06 22:31:10 +0530

    Add files via upload
    function[optpara] = crossvalidation(foldabledata,otherpara,parameterlist,train_func,test_func, prfrmncePara,fold)
    
     foldabledata: An array of size (1 x Number of Classes), at each index a 2-dim array of input samples having features and the target value is kept in the structure's data member 'data'
     Example: foldabledata(1).data=[rand(100,4) 2*ones(100,1)]
      
    fold: parameter k of k-fold cross-validation
    Example: fold=5 for 5-fold cross-validation
    
    parameterlist: An array of structures of parameters. The optimal value of these parameters needs to be found by grid search over all possible combinations of the parameter values using k-fold cross-validation by minimising val.err
    Example: parameterlist(1).values = [1,2,3]; parameterlist(2).values = [4,7,8,3];
    
    otherpara: structure having all other required parameters be passed to trainfunc or testfunc functions. These parameters do not play any role in cross-validation. These will be passed exactly in the same way. 
    
    train_func: handle to train function returns out structure which will be feeded to testfunc. out structure keeps all the information generated during training the model and is required to generate predictions.
    The train_func should follow following prototype:
             [Mdl] = train_func(trainfold,otherpara,parameter)
    where trainfold: is exactly of the same type as that of foldabledata
              otherpara: same as that of otherpara of crossvalidation
              parameter: is an array of hyperparameter values. The order will be exactly the same as that of parameterlist, example: parameter = [parameterlist(1).value(1), parameterlist(2).value(1),...]
               -----------------------------------------------------
             Mdl: An structure containing all the information generated by train_func but required by prediction function to generate prediction on a given test sample. 
    
    test_func: handle to predict function, returns structure containing an PC field, considered for predicted value and act field for actual target values. It should have following prototype
               [res] = test_func(trainfold,testfold,parameter,Mdl,otherpara);
               trainfold, testfold: has same structure as that of foldabledata
               parameter: is an array of hyperparameter values. The order will be exactly the same as that of parameterlist, example: parameter = [parameterlist(1).value(1), parameterlist(2).value(1),...]
               Mdl: An structure containing all the information generated by train_func but required by prediction function to generate prediction on a given test sample. 
               otherpara: same as that of otherpara of cross-validation
               -----------------------------------------------------
               res: is a structure with two fields. act for the actual value of the target variable and PC for the predicted value of the target variable.
    
    prfrmncePara: handle to performance parameter evaluation function on  the lower value of which hyperparameter is tuned. It should output a structure with an attribute err treated as error. It should have following prototype:
               [val] = prfrmncePara(act,PC)
               where  act for the actual value of the target variable and PC for the predicted value of the target variable.
               -------------------------------   
               val is a structure with err as one of the attribute. Hyperparameters will be chosen by minimising this value 
    ---------------------------------------------------------
    Output:
    optpara.parameter- structure that keeps parameter values asked to tuned for
    optpara.error-structure that keeps values of error corresponding to each specific set of parameter values asked to tuned for 
    
    ---------------------------------------------------------
    Author: Kapil 
    Affiliation: Department of Computer Applications, NIT Kurukshetra
    Email: kapil@nitkkr.ac.in

---
+++ crossvalidation.m
@@ -0,0 +1,71 @@
+function[optpara] = crossvalidation(foldabledata,otherpara,parameterlist,train_func,test_func, prfrmncePara,fold)
+%--------------------------------------------------------------------------
+%  foldabledata: A structure array, at each index a 2-dim array of input 
+%                samples having features and target value with attribute
+%                .data
+%   example: foldabledata(1).data=[rand(100,4) 2*ones(100,1)]
+%  
+%  fold: parameter k of k-fold crossvalidation
+%  
+%  otherpara: structure of all other parameters needs to be passed to
+%               trainfunc or testfunc functions, these will be passed
+%               exactly in same way
+%  
+%  parameterlist: structure of parameters on which crossvalidaion to be
+%               performed (parameter values are kept as in below example)
+%       parameterlist(1).values = [1,2,3];
+%       parameterlist(2).values = [4,7,8,3];
+%  
+%  train_func : handle to train function returns out structure which will be
+%               feeded to testfunc
+%
+%  test_func  : handle to predict function, returns structure containing an 
+%               PC field, considered for predicted value and act field for
+%               actual target values
+%
+%  prfrmncePara: handle to preformance parameter evaluation function on 
+%                the lower value of which hyperparameter is tuned. 
+%                It should output a structure with attribute err
+%--------------------------------------------------------------------------
+%Output:
+%   optpara.parameter- structure that keeps parameter values asked to tuned for
+%   optpara.error-structure that keeps values of error corresponding to each
+%                  specific set of parameter values asked to tuned for 
+% Author: Kapil Email: Kapil@nitkkr.ac.in
+%--------------------------------------------------------------------------
+minerr = Inf;
+nloop = 1;
+for i = 1:size(parameterlist,2)
+    nloop = nloop*size(parameterlist(i).values,2);
+end
+index = zeros(1,size(parameterlist,2));
+parameter = zeros(nloop,size(parameterlist,2));
+
+
+for i = 0:nloop-1
+    temp = i;
+    for j = size(parameterlist,2):-1:1
+        index(j) = mod(temp,size(parameterlist(j).values,2))+1;
+        temp = floor(temp/size(parameterlist(j).values,2));
+        parameter(i+1,j) = parameterlist(j).values(index(j));
+    end
+end
+error = zeros(nloop,1);
+parfor i = 0:nloop-1
+    parameter(i+1,:)
+    for j = 1:fold
+        [trainfold,testfold] = blocking(foldabledata,fold,j);
+                
+        [Mdl] = train_func(trainfold,otherpara,parameter(i+1,:));
+        [res] = test_func(trainfold,testfold,parameter(i+1,:),Mdl,otherpara);
+
+        [val] = prfrmncePara(res.act,res.PC);
+        error(i+1) = error(i+1) + val.err;
+    end
+end
+[~,indx] = min(error);
+optpara.para = parameter(indx,:);
+optpara.minerr = error(indx);
+optpara.parameter = parameter;
+optpara.error = error;
+%optpara.Mdl = Mdl;
