b988347477ffaf94b0994a28e506c00d7c92cc21
Kapil Gupta <navkapil@gmail.com>
2024-03-07 11:23:49 +0530

    Update rbf_kernel.m
    Some comment added

--- rbf_kernel.m
+++ rbf_kernel.m
@@ -1,4 +1,7 @@
 function [ker] = rbf_kernel(A,B,mu)
+% function or kernel returns similarity between two vectors(between 0 to 1), the first one is a row of A another
+% is a row of B, higher value of mu quickly reduces the similarity value to zero as the two vectors go farther 
+% and lower value of mu, flattens the similarity curve, giving rise to more smoother boundaries.
     m1 = size(A,1);
     m2 = size(B,1);
     ker = zeros(m1,m2);
