9ff4bbb6266744885ad5d66b4d309ded0c128cde
Kapil Gupta <navkapil@gmail.com>
2024-03-07 11:31:43 +0530

    Useless lines omitted from blocking.m
    Some useless code omitted, which is also commented

--- crossvalidation/blocking.m
+++ crossvalidation/blocking.m
@@ -21,24 +21,12 @@ function [trainfold,testfold] = blocking(foldabledata,fold,i)
     for j=1:numclass
         data = foldabledata(j).data; % the data structure as managed by seprate_class
         samples = size(data,1);
-        % ori_index = foldabledata(j).ori_index;
-        % count = foldabledata(j).count;
-        % label = foldabledata(j).label;
 
         blocksz = samples/fold;
         teststart = floor(blocksz*(i-1)+1);
         testend = floor(blocksz*(i));
         
         testfold(j).data = data(teststart:testend,:);
-        
-        % testfold(j).label = foldabledata(j).label;
-        % testfold(j).count = testend-teststart+1;
-        % testfold(j).ori_index = ori_index(teststart:testend,:);
-
         trainfold(j).data = [data(1:teststart-1,:);data(testend+1:samples,:)];
-        
-        % trainfold(j).label = label;
-        % trainfold(j).count = count-testfold(j).count;
-        % trainfold(j).ori_index = [ori_index(1:teststart-1,:);ori_index(testend+1:samples,:)];
     end 
 end
